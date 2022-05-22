8aecd8903df366bd4509dd5849c8a6b106277f7b
KirubelEnyew <kirastarboys@gmail.com>
2022-05-22 20:31:28 +0300

    feat - added nav bar component

---
+++ src/Components/NavigationBar/NavBar.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { Appbar, AppBarText, Button } from '../StyledComponents/StyledComponents'
+
+const NavBar = () => {
+  const location = useLocation()
+  const navigate = useNavigate()
+  const isOnAddRoute = () => {
+    if (location.pathname === '/') return false
+
+    return true
+  }
+  return (
+    <Appbar >
+      <AppBarText onClick={() => { navigate('/') }}>Addis Software: Test Project</AppBarText>
+      <Button
+        variant='filled'
+        colorType='success'
+        onClick={() => { isOnAddRoute() ? navigate('/') : navigate('/new-employee') }}
+        width='150px'>
+        {isOnAddRoute() ? '< Go back' : '+ Add Employee'}
+      </Button>
+    </Appbar>
+  )
+}
+
+export default NavBar
