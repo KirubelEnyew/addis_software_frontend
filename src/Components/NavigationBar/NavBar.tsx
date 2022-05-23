cff6f56d096362dbf74e3fbdc904770ac89b354b
KirubelEnyew <kirastarboys@gmail.com>
2022-05-23 18:58:54 +0300

    style - removed add button from navigation

--- src/Components/NavigationBar/NavBar.tsx
+++ src/Components/NavigationBar/NavBar.tsx
@@ -1,25 +1,12 @@
 import React from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
-import { Appbar, AppBarText, Button } from '../StyledComponents/StyledComponents'
+import { useNavigate } from 'react-router-dom'
+import { Appbar, AppBarText } from '../StyledComponents/StyledComponents'
 
 const NavBar = () => {
-  const location = useLocation()
   const navigate = useNavigate()
-  const isOnAddRoute = () => {
-    if (location.pathname === '/') return false
-
-    return true
-  }
   return (
     <Appbar >
       <AppBarText onClick={() => { navigate('/') }}>Addis Software: Test Project</AppBarText>
-      <Button
-        variant='filled'
-        colorType='success'
-        onClick={() => { isOnAddRoute() ? navigate('/') : navigate('/employee-form') }}
-        width='150px'>
-        {isOnAddRoute() ? '< Go back' : '+ Add Employee'}
-      </Button>
     </Appbar>
   )
 }
